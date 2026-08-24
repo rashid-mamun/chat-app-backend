7e83e6ba3e4d871565afbddc45e39b642827fc9c
rashid-mamun <rashid@sharetrip.net>
2026-08-24 11:53:47 +0600

    chore: remove stale docs and dead code

--- src/config/socket.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    socketConfig: {
-        pingTimeout: 60000,
-        pingInterval: 25000,
-        maxHttpBufferSize: 1e8
-    }
-};
--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    CHAT_TYPES: {
-        PRIVATE: 'private',
-        GROUP: 'group'
-    },
-    FILE_TYPES: ['image', 'video', 'audio', 'document', 'other'],
-    REACTION_TYPES: ['like', 'love', 'laugh', 'sad', 'angry']
-};
